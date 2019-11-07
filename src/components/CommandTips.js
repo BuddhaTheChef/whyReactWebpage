a985a8308bb814efd09fd7f7db02532f449d7ef8
rforest11 <55857525+rforest11@users.noreply.github.com>
2019-11-07 09:30:57 -0800

    updated command tips
    added my part of the command tips page

--- src/components/CommandTips.js
+++ src/components/CommandTips.js
@@ -5,67 +5,50 @@ export default class CommandTips extends Component {
         return (
             <div>
                 <h1 style={{textAlign: 'center', marginTop: '80px', fontSize: '70px'}}>Command Tips</h1>
+               <div>
+                <h1 style={{textAlign: 'center', marginTop: '80px', fontSize: '70px'}}>Installing reactjs tutorial</h1>
                 <div className="gettingStartedVideoDiv">
-                <iframe title="reactvid" width="920" height="555" src="https://youtu.be/xa-_FIy2NgE" frameborder="0" allow="accelerometer; autoplay=1; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/aVPOoFic1i4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
+              </div>
                 <div className="gettingStartedDivPic">
-                    Insert Pic here
+                    
                 </div>
                 <div className="gettingStartedDiv">
                 <h3>Step 1</h3>
                     <p>
-                    . Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen 
-                    book. It has survived not only five centuries, but also the leap into electronic typesetting,
-                     remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-                     sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-                      Aldus PageMaker including versions of Lorem Ipsum.
+                    Install Nodejs. Nodejs will be critical into using React. while you download Nodejs you can create a folder for your project. Gatsby is another program you can use if you dont want to use Nodejs but this tutorial is going to be using Nodejs so i reccomend using Nodejs
                     </p>
                 </div>
                 <div className="gettingStartedDivPic">
-                    Insert Pic Here
+                    
                 </div>
                 <div className="gettingStartedDiv">
                 <h3>Step 2</h3>
                     <p>
-                    . Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen 
-                    book. It has survived not only five centuries, but also the leap into electronic typesetting,
-                     remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-                     sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-                      Aldus PageMaker including versions of Lorem Ipsum.
+                    Next you would want to Install the reactjs modeuels using the command "npm install -g create react app". Once youve done that use the command "create-react-app " this command will check to see if reactjs is installed properly  
                     </p>
                 </div>
                 <div className="gettingStartedDivPic">
-                    Insert Pic Here
+                    
                 </div>
                 <div className="gettingStartedDiv">
                 <h3>Step 3</h3>
                     <p>
-                    . Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen 
-                    book. It has survived not only five centuries, but also the leap into electronic typesetting,
-                     remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-                     sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-                      Aldus PageMaker including versions of Lorem Ipsum.
+                    You know want to create a new reactjs app using the command "create-react-app new app".Once you have done that you now go the project folder and use the command"cd new-app"
                     </p>
                 </div>
                 <div className="gettingStartedDivPic">
-                    Insert Pic Here
+                    
                 </div>
                 <div className="gettingStartedDiv">
                 <h3>Step 4</h3>
                     <p>
-                    . Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen 
-                    book. It has survived not only five centuries, but also the leap into electronic typesetting,
-                     remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-                     sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-                      Aldus PageMaker including versions of Lorem Ipsum.
+                   Now you can start the server using the command "npm start". Now modify the App.js file and run it. Congratualtions you have now Installed reactjs 
                     </p>
                 </div>
                 <div className="gettingStartedDivPic">
-                    Insert Pic Here
+                    
                 </div>
             
               <div>
