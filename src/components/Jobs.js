6edffdb447e6fb5aa759d4eecd3455885a2259bd
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-11-08 10:32:08 -0800

    added in jobs page

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,11 +1,62 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Jobs extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Jobs</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1 style={{textAlign: 'center', fontSize: '50px'}}>Jobs</h1>
+        <div style={{display: 'flex', justifyContent: 'space-around'}}>
+              <div style={{display: 'inline-flex', background: 'gray', height: '350px', width:'18%'}}>stuff</div>
+              <div style={{display: 'inline-flex', background: 'gray', height: '350px', width:'18%'}}>stuff</div>
+              <div style={{display: 'inline-flex', background: 'gray', height: '350px', width:'18%'}}>stuff</div>
+              <div style={{display: 'inline-flex', background: 'gray', height: '350px', width:'18%'}}>stuff</div>
+              <div style={{display: 'inline-flex', background: 'gray', height: '350px', width:'18%'}}>stuff</div>
+          </div>
+          <div style={{display: 'flex', justifyContent: 'space-around', marginTop: '20px'}}>
+              <div style={{display: 'inline-flex', background: 'gray', height: '350px', width:'18%'}}>stuff</div>
+              <div style={{display: 'inline-flex', background: 'gray', height: '350px', width:'18%'}}>stuff</div>
+              <div style={{display: 'inline-flex', background: 'gray', height: '350px', width:'18%'}}>stuff</div>
+              <div style={{display: 'inline-flex', background: 'gray', height: '350px', width:'18%'}}>stuff</div>
+              <div style={{display: 'inline-flex', background: 'gray', height: '350px', width:'18%'}}>stuff</div>
+          </div>
+        <div style={{color:'#05d9fe',background: 'slategray', width: '80%', display: 'flex', alignItems: 'center', margin: '50px auto', padding: '30px', lineHeight: '2', fontSize: '20px', borderRadius: '4px', border: 'whitesmoke 1px solid', textAlign: 'center', fontWeight: 'bolder'}}>
+          The average React Js Developer salary in the USA is $125,000 per year
+          or $64.10 per hour. Entry-level positions start at $63,050 per year
+          while most experienced workers make up to $195,000 per year.
+          </div>
+          <div style={{background: 'slategray', width: '80%', display: 'flex', alignItems: 'center', margin: '50px auto', padding: '30px', lineHeight: '2', fontSize:'20px', borderRadius: '4px', border: 'whitesmoke 1px solid'}}>
+           React on
+          and by itself is a fast thing to learn, especially considering you are
+          a developer or at least have some familiarity with writing code.
+         </div>
+          <div style={{background: 'slategray', width: '80%', display: 'flex', alignItems: 'center', margin: '50px auto', padding: '30px', lineHeight: '2', flexDirection: 'column', borderRadius: '4px', border: 'whitesmoke 1px solid'}}>
+          <h3>What does a React developer do?</h3>
+          React developer will be creating the
+          interactive part of the web, by creating components that can send and
+          receive data from a backend server. They will be responsible for the
+          rendering of components which is having customized requirements.
+          </div>
+          <div style={{width: '100%', display: 'flex'}}>
+          <div style={{background: 'slategray', width: '33%', display: 'inline-flex', alignItems: 'center', margin: '50px auto', padding: '30px', lineHeight: '2', flexDirection: 'column', borderRadius: '4px', border: 'whitesmoke 1px solid'}}>
+          <h3>How long will it take?</h3>
+          The short answer is it will take you between
+          1-12 months of learning to become proficient with React. This depends
+          on your existing software development experience, your knowledge of
+          javascript and the learning path you take. The following report is based on
+          39,441 responses. In short, 33 percent of employers need to have React
+          skills, but only 19 percent of developers said they had known React.
+          </div>
+          <div style={{background: 'slategray', width: '33%', display: 'inline-flex', alignItems: 'center', margin: '50px auto', padding: '30px', lineHeight: '2', flexDirection: 'column', borderRadius: '4px', border: 'whitesmoke 1px solid'}}>
+          <h3>FACTS</h3> 
+          <ul>
+              <li>Since React is a JS library, there is no way around learning JavaScript.</li>
+              <li>React makes heavily use of JavaScript.</li>
+              <li>React JS is known to be one of the most widely used JavaScript libraries.</li>
+              <li> According to SimilarTech, it's now being used on more than 90,000 websites.</li>
+          </ul> 
+          </div>
+          </div>
+      </div>
+    );
+  }
 }
