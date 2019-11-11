a3423bf9915e90d62af8fdade1468e19f2447c3d
Venus Aoki <55719452+venusaoki@users.noreply.github.com>
2019-11-10 18:38:39 -0800

    Add files via upload

---
+++ src/README.md
@@ -0,0 +1,3 @@
+# itc134project1-wttu1q
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/itc134project1-wttu1q)
--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './style.css';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+
+        <p>
+
+        </p>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
