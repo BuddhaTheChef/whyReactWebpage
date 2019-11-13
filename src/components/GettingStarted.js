8b309df28730266148e688d1400e15e5960132be
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-11-12 18:08:11 -0800

    getting started divs

--- src/components/GettingStarted.js
+++ src/components/GettingStarted.js
@@ -1,60 +1,94 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class GettingStarted extends Component {
-    render() {
-        return (
-            <div>
-                <h1 className="GettingStarted">
-                Getting Started
-                </h1>
+  render() {
+    return (
+      <div>
+        <h1 className="GettingStarted">Getting Started</h1>
 
-                <div>
-
-                <div className="gettingStartedDivPic">
-                    Insert Pic Here
-                </div>
-                <div className="gettingStartedDiv">
-                <h3>Step 6</h3>
-                    <p>
-                    . Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen 
-                    book. It has survived not only five centuries, but also the leap into electronic typesetting,
-                     remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-                     sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-                      Aldus PageMaker including versions of Lorem Ipsum.
-                    </p>
-                </div>
-                <div className="gettingStartedDivPic">
-                    Insert Pic Here
-                </div>
-                <div className="gettingStartedDiv">
-                <h3>Step 7</h3>
-                    <p>
-                    . Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen 
-                    book. It has survived not only five centuries, but also the leap into electronic typesetting,
-                     remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-                     sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-                      Aldus PageMaker including versions of Lorem Ipsum.
-                    </p>
-                </div>
-                <div className="gettingStartedDivPic">
-                    Insert Pic Here
-                </div>
-                <div className="gettingStartedDiv">
-                <h3>Step 8</h3>
-                    <p>
-                    . Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen 
-                    book. It has survived not only five centuries, but also the leap into electronic typesetting,
-                     remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-                     sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-                      Aldus PageMaker including versions of Lorem Ipsum.
-                    </p>
-                </div>
-                </div>
-              
+        <div>
+          <div className="gettingStartedMainDiv">
+            <div className="gettingStartedDivPic1">Insert Pic Here</div>
+            <div className="gettingStartedDiv1">
+              <h3>Step 1</h3>
+              <p>
+                . Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s, when an unknown printer took a galley of type
+                and scrambled it to make a type specimen book. It has survived
+                not only five centuries, but also the leap into electronic
+                typesetting, remaining essentially unchanged. It was popularised
+                in the 1960s with the release of Letraset sheets containing
+                Lorem Ipsum passages, and more recently with desktop publishing
+                software like Aldus PageMaker including versions of Lorem Ipsum.
+              </p>
             </div>
-        )
-    }
+          </div>
+          <div className="gettingStartedMainDiv">
+            <div className="gettingStartedDivPic1">Insert Pic Here</div>
+            <div className="gettingStartedDiv1">
+              <h3>Step 2</h3>
+              <p>
+                . Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s, when an unknown printer took a galley of type
+                and scrambled it to make a type specimen book. It has survived
+                not only five centuries, but also the leap into electronic
+                typesetting, remaining essentially unchanged. It was popularised
+                in the 1960s with the release of Letraset sheets containing
+                Lorem Ipsum passages, and more recently with desktop publishing
+                software like Aldus PageMaker including versions of Lorem Ipsum.
+              </p>
+            </div>
+          </div>
+          <div className="gettingStartedMainDiv">
+            <div className="gettingStartedDivPic1">Insert Pic Here</div>
+            <div className="gettingStartedDiv1">
+              <h3>Step 3</h3>
+              <p>
+                . Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s, when an unknown printer took a galley of type
+                and scrambled it to make a type specimen book. It has survived
+                not only five centuries, but also the leap into electronic
+                typesetting, remaining essentially unchanged. It was popularised
+                in the 1960s with the release of Letraset sheets containing
+                Lorem Ipsum passages, and more recently with desktop publishing
+                software like Aldus PageMaker including versions of Lorem Ipsum.
+              </p>
+            </div>
+          </div>
+          <div className="gettingStartedMainDiv">
+            <div className="gettingStartedDivPic1">Insert Pic Here</div>
+            <div className="gettingStartedDiv1">
+              <h3>Step 4</h3>
+              <p>
+                . Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s, when an unknown printer took a galley of type
+                and scrambled it to make a type specimen book. It has survived
+                not only five centuries, but also the leap into electronic
+                typesetting, remaining essentially unchanged. It was popularised
+                in the 1960s with the release of Letraset sheets containing
+                Lorem Ipsum passages, and more recently with desktop publishing
+                software like Aldus PageMaker including versions of Lorem Ipsum.
+              </p>
+            </div>
+          </div>
+          <div className="gettingStartedMainDiv">
+            <div className="gettingStartedDivPic1">Insert Pic Here</div>
+            <div className="gettingStartedDiv1">
+              <h3>Step 5</h3>
+              <p>
+                . Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s, when an unknown printer took a galley of type
+                and scrambled it to make a type specimen book. It has survived
+                not only five centuries, but also the leap into electronic
+                typesetting, remaining essentially unchanged. It was popularised
+                in the 1960s with the release of Letraset sheets containing
+                Lorem Ipsum passages, and more recently with desktop publishing
+                software like Aldus PageMaker including versions of Lorem Ipsum.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
